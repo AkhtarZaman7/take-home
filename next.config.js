1efc4adee030945547779938400bfe362263fbf8
AkhtarZaman12 <akhtar.tarar12@gmail.com>
2023-11-18 07:19:32 -0500

    refactor code

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     return [
       {
         source: '/',
-        destination: '/log10',
+        destination: '/chat',
         permanent: true,
       },
     ];
--- src/app/page.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-export default function Home() {
-  // const {theme, setTheme} = useTheme();
-  return <div className='flex'></div>;
-}
